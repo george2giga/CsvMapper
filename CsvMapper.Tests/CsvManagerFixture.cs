15e49b88dd559826c7daa0d07baf6f60f93e2c66
Giorgio Minardi <MINARDG@LT034894>
2016-07-22 14:07:06 +0100

    * added customer test

--- CsvMapper.Tests/CsvManagerFixture.cs
+++ CsvMapper.Tests/CsvManagerFixture.cs
@@ -11,20 +11,23 @@ namespace CsvMapper.Tests
     [TestFixture]
     class CsvManagerFixture
     {
-        protected readonly string FileName = "USPresident_Wikipedia.csv";
+        protected readonly string PresidentsFileName = "USPresident_Wikipedia.csv";
+        protected readonly string CustomersFileName = "Customer_data.csv";
         protected readonly string RootFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestFiles");
-        private string _fullFilePath;
+        private string _presidentsfullFilePath;
+        private string _customersfullFilePath;
 
         [SetUp]
         public void Setup()
         {
-            _fullFilePath = Path.Combine(RootFolder, FileName);
+            _presidentsfullFilePath = Path.Combine(RootFolder, PresidentsFileName);
+            _customersfullFilePath = Path.Combine(RootFolder, CustomersFileName);
         }
 
         [Test]
         public void Load_Us_Presidents_Test()
         {
-            var presidentsManager = new CsvManager<UsPresident>(_fullFilePath);
+            var presidentsManager = new CsvManager<UsPresident>(_presidentsfullFilePath);
             presidentsManager.SetField(x => x.PresidencyId, 0);
             presidentsManager.SetField(x => x.President, 1);
             presidentsManager.SetField(x => x.WikipediaEntry, 2);
@@ -41,10 +44,14 @@ namespace CsvMapper.Tests
         }
 
         [Test]
-        public void Load_People_Test_No_Autoset()
+        public void Load_Customer_Test_No_Autoset()
         {
-            
-        }
+            var customersManager = new CsvManager<Customer>(_customersfullFilePath);
 
+            var customersResult = customersManager.GetObjectList();
+
+            Assert.IsNotNull(customersResult);
+            Assert.IsTrue(customersResult.Count > 0);
+        }
     }
 }
--- CsvMapper.Tests/Customer.cs
+++ CsvMapper.Tests/Customer.cs
@@ -8,5 +8,13 @@ namespace CsvMapper.Tests
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Ip_address { get; set; }
+        public string City { get; set; }
+        public long? CreditCard { get; set; }
     }
 }
